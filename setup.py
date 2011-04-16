610b2fe554d8b7cf3245a3a17a3525ab87949a64
Luke Hodkinson <furious.luke@gmail.com>
2011-04-17 09:45:15 +1000

    Adding a few odds and ends.

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name='django-address',
+    version='0.1',
+    url='https://github.com/furious-luke/django-address',
+    license='',
+    descriptions='A django application for describing addresses.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author='Luke Hodkinson',
+    author_email='furious.luke@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['*.txt', '*.js', '*.html', '*.*']},
+    install_requires=['setuptools'],
+    classifiers = [
+        'Framework :: Django',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python'
+    ]
+)
