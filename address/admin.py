36f953eb4738822c8cb08ac964fca50cb6258242
Luke Hodkinson <furious.luke@gmail.com>
2011-04-22 07:28:50 +0000

    Adding an admin file.

---
+++ address/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+import models
+
+
+class CountryAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'code')
+admin.site.register(models.Country, CountryAdmin)
+
+
+class StateAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'code')
+admin.site.register(models.State, StateAdmin)
+
+
+class LocalityAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'postal_code')
+admin.site.register(models.Locality, LocalityAdmin)
+
+
+class AddressAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+admin.site.register(models.Address, AddressAdmin)
