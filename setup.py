fe390ecc7150d63aae11791e973919d4901936d1
Luke Hodkinson <furious.luke@gmail.com>
2011-04-22 07:28:29 +0000

    Cleaning up the setup file.

--- setup.py
+++ setup.py
@@ -4,19 +4,27 @@ from setuptools import setup, find_packages
 setup(
     name='django-address',
     version='0.1',
-    url='https://github.com/furious-luke/django-address',
-    license='BSD',
-    description='A django application for describing addresses.',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author='Luke Hodkinson',
     author_email='furious.luke@gmail.com',
+    maintainer='Luke Hodkinson',
+    maintainer_email='furious.luke@gmail.com',
+    url='https://github.com/furious-luke/django-address',
+    description='A django application for describing addresses.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    license='BSD',
+
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['*.txt', '*.js', '*.html', '*.*']},
     install_requires=['setuptools'],
-    classifiers = [
-        'Framework :: Django',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python'
-    ]
+
 )
