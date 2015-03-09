a1c968d9ff56e26ca18191845e6e7cb5fef0edeb
Luke Hodkinson <furious.luke@gmail.coim>
2015-03-10 06:21:41 +1100

    Adding an example test for modifying admin widgets.

--- example_site/example/admin.py
+++ example_site/example/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from address.forms import AddressField, AddressWidget
+from address.models import AddressField
+from address.forms import AddressWidget
 from .models import Example
 
 class ExampleAdmin(admin.ModelAdmin):
     formfield_overrides = {
-        AddressField: {'widget': AddressWidget}
+        AddressField: {'widget': AddressWidget(attrs={'style': 'width: 300px;'})}
     }
 
 admin.site.register(Example, ExampleAdmin)
