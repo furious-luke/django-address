1e509436bc081a4915f5eec78fd61a1854bb1c52
Luke Hodkinson <furious.luke@gmail.coim>
2015-10-27 23:28:01 +1100

    Found a couple more improvements to the hierarchy.

--- address/hierarchy.py
+++ address/hierarchy.py
@@ -2,7 +2,7 @@ from .kinds import *
 
 hierarchy = {
     KIND_STREET_ADDRESS:  [KIND_ROUTE],
-    KIND_ROUTE:           [KIND_SUBPREMISE],
+    KIND_ROUTE:           [KIND_POSTAL_CODE_PREFIX],
     KIND_INTERSECTION:    [KIND_NEIGHBORHOOD],
     KIND_POLITICAL:       [],
     KIND_AAL1:            [KIND_COUNTRY],
