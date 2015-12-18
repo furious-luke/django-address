bb623deda070c6e6c917941daea2aeac2101aec3
Matt <h.dudeness@gmail.com>
2015-12-18 07:38:51 -0600

    move jquery code to plugin

--- address/static/address/js/address.js
+++ address/static/address/js/address.js
@@ -1,42 +1,49 @@
-function django_input_address_setup() {
-    $('input.address').each(function() {
-        var self = $(this);
-        //Skip if already setup
-        if (self.data('django_input_address_setup')) {
-            return true;
-        }
+(function($) {
 
-        var cmps = $('#' + self.attr('name') + '_components');
-        var fmtd = $('input[name="' + self.attr('name') + '_formatted"]');
-        self.geocomplete({
-            details: cmps,
-            detailsAttribute: 'data-geo'
-        }).change(function() {
-            if (self.val() != fmtd.val()) {
-                var cmp_names = [
-                    'country',
-                    'country_code',
-                    'locality',
-                    'postal_code',
-                    'route',
-                    'street_number',
-                    'state',
-                    'state_code',
-                    'formatted',
-                    'latitude',
-                    'longitude'
-                ];
-                for (var ii = 0; ii < cmp_names.length; ++ii)
-                    $('input[name="' + self.attr('name') + '_' + cmp_names[ii] + '"]').val('');
+    $.fn.djangoAddressInput = function() {
+
+        this.each(function() {
+            var self = $(this);
+            //Skip if already setup
+            if (self.data('django_input_address_setup')) {
+                return true;
             }
+
+            var cmps = $('#' + self.attr('name') + '_components');
+            var fmtd = $('input[name="' + self.attr('name') + '_formatted"]');
+            self.geocomplete({
+                details: cmps,
+                detailsAttribute: 'data-geo'
+            }).change(function() {
+                if (self.val() != fmtd.val()) {
+                    var cmp_names = [
+                        'country',
+                        'country_code',
+                        'locality',
+                        'postal_code',
+                        'route',
+                        'street_number',
+                        'state',
+                        'state_code',
+                        'formatted',
+                        'latitude',
+                        'longitude'
+                    ];
+                    for (var ii = 0; ii < cmp_names.length; ++ii)
+                        $('input[name="' + self.attr('name') + '_' + cmp_names[ii] + '"]').val('');
+                }
+            });
+
+            //Add data so element will not be setup again
+            self.data('django_input_address_setup', true);
         });
 
-        //Add data so element will not be setup again
-        self.data('django_input_address_setup', true);
-    });
-}
+        return this;
 
+    };
+
+}(jQuery));
 
 $(function() {
-	django_input_address_setup();
+	$('input.address').djangoAddressInput();
 });
