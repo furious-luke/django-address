18e21a98db54937c14a44929586b635e33adeae4
Matt <h.dudeness@gmail.com>
2015-12-17 19:31:41 -0600

    move address input initialize to function so it can be called if new elements are added

--- address/static/address/js/address.js
+++ address/static/address/js/address.js
@@ -1,19 +1,42 @@
-$(function(){
-    $('input.address').each(function(){
+function django_input_address_setup() {
+    $('input.address').each(function() {
         var self = $(this);
-	var cmps = $('#' + self.attr('name') + '_components');
-	var fmtd = $('input[name="' + self.attr('name') + '_formatted"]');
+        //Skip if already setup
+        if (self.data('django_input_address_setup')) {
+            return true;
+        }
+
+        var cmps = $('#' + self.attr('name') + '_components');
+        var fmtd = $('input[name="' + self.attr('name') + '_formatted"]');
         self.geocomplete({
             details: cmps,
             detailsAttribute: 'data-geo'
-        }).change(function(){
-	    if(self.val() != fmtd.val()) {
-		var cmp_names = ['country', 'country_code', 'locality', 'postal_code',
-				 'route', 'street_number', 'state', 'state_code',
-				 'formatted', 'latitude', 'longitude'];
-		for(var ii = 0; ii < cmp_names.length; ++ii)
-		    $('input[name="' + self.attr('name') + '_' + cmp_names[ii] + '"]').val('');
-	    }
-	});
+        }).change(function() {
+            if (self.val() != fmtd.val()) {
+                var cmp_names = [
+                    'country',
+                    'country_code',
+                    'locality',
+                    'postal_code',
+                    'route',
+                    'street_number',
+                    'state',
+                    'state_code',
+                    'formatted',
+                    'latitude',
+                    'longitude'
+                ];
+                for (var ii = 0; ii < cmp_names.length; ++ii)
+                    $('input[name="' + self.attr('name') + '_' + cmp_names[ii] + '"]').val('');
+            }
+        });
+
+        //Add data so element will not be setup again
+        self.data('django_input_address_setup', true);
     });
+}
+
+
+$(function() {
+	django_input_address_setup();
 });
