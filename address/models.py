50bf3ba7a338c07cab0d3c8b05407d9af2e1042b
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-12-29 17:41:03 +0000

    Update descriptor for django 1.9

--- address/models.py
+++ address/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.exceptions import ValidationError
-from django.db.models.fields.related import ForeignObject, ReverseSingleRelatedObjectDescriptor
+from django.db.models.fields.related import ForeignObject
+from django.db.models.fields.related_descriptors import ForwardManyToOneDescriptor
 from django.utils.encoding import python_2_unicode_compatible
 
 import logging
@@ -269,7 +270,7 @@ class Address(models.Model):
                     ad['country_code'] = self.locality.state.country.code
         return ad
 
-class AddressDescriptor(ReverseSingleRelatedObjectDescriptor):
+class AddressDescriptor(ForwardManyToOneDescriptor):
 
     def __set__(self, inst, value):
         super(AddressDescriptor, self).__set__(inst, to_python(value))
