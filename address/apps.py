f16506033ec6180a63646b4c6b2a0c68f1b05b61
Dave Brown <wakeb0ardn@gmail.com>
2017-05-12 23:34:55 -0400

    Create apps.py

---
+++ address/apps.py
@@ -0,0 +1,11 @@
+from django.apps import AppConfig
+
+
+class AddressConfig(AppConfig):
+    """
+    Define config for the member app so that we can hook in signals.
+    """
+    name = 'address'
+
+    def ready(self):
+        import signals
