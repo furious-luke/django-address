dda414d5bcfc3f3f6c459a975f57e647cf93b8de
Aaron Kim <ahkim@ahkim.com>
2017-10-28 08:25:20 +0800

    Update urlpattern for home to be Django 1.10+ compatible

--- example_site/example_site/urls.py
+++ example_site/example_site/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from example.views import home
 
 urlpatterns = [
-    url(r'^$', 'example.views.home', name='home'),
+    url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
