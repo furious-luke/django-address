7fa40a4a99661b028c333d50ced837ae5dd6bdf7
Ali Karbassi <ali@karbassi.com>
2018-04-01 15:48:05 -0500

    Sensor not required.
    From Google Docs:
    > The `sensor` parameter is no longer required for the Google Maps
    > JavaScript API. It won't prevent the Google Maps JavaScript API from
    > working correctly, but we recommend that you remove the `sensor`
    > parameter from the script element.
    
    https://developers.google.com/maps/documentation/javascript/error-messages#sensor-not-required

--- address/widgets.py
+++ address/widgets.py
@@ -28,7 +28,7 @@ class AddressWidget(forms.TextInput):
     class Media:
         """Media defined as a dynamic property instead of an inner class."""
         js = [
-            'https://maps.googleapis.com/maps/api/js?libraries=places&sensor=false&key=%s' % settings.GOOGLE_API_KEY,
+            'https://maps.googleapis.com/maps/api/js?libraries=places&key=%s' % settings.GOOGLE_API_KEY,
             'js/jquery.geocomplete.min.js',
             'address/js/address.js',
         ]
